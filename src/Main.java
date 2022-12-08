26820913e2bb89f59d8af79965ac7ab6c12b3ca9
anna <anna@sayf.su>
2022-12-08 13:09:55 +0300

    task 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int p = 0;
+        if (p % 2 !=0 || p == 0) {
+            System.out.println("Число " + p + " нечетное число");
+        } else {
+            System.out.println("Число " + p + " четное число");
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        float number = 10;
+        float p = 7.3f;
+        float d = 11.13f;
+
+        if (Math.abs(p - number) > Math.abs(d - number)) {
+            System.out.println("Число " + d + " ближайшее к числу " + number);
+        } else {
+            System.out.println("Число " + p + " ближайшее к числу " + number);
+        }
     }
 }
